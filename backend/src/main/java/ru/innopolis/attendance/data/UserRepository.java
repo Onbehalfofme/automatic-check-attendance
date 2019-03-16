8b3a7f4607f0e50925745f23dd089ead724fe401
Maxim Salo <m.salo@innopolis.ru>
2019-03-16 19:40:37 +0300

    Edit user repository

--- backend/src/main/java/ru/innopolis/attendance/data/UserRepository.java
+++ backend/src/main/java/ru/innopolis/attendance/data/UserRepository.java
@@ -2,8 +2,11 @@ package ru.innopolis.attendance.data;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.innopolis.attendance.models.User;
+import ru.innopolis.attendance.models.InnoUser;
+
+import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long>{
+public interface UserRepository extends JpaRepository<InnoUser, Long> {
+    Optional<InnoUser> findByEmail(String email);
 }
