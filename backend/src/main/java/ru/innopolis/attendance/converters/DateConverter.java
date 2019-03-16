e51cff03300a104309973ebe825b1a4493a66f32
Maxim Salo <m.salo@innopolis.ru>
2019-03-16 19:41:04 +0300

    Add date and time converters

---
+++ backend/src/main/java/ru/innopolis/attendance/converters/DateConverter.java
@@ -0,0 +1,18 @@
+package ru.innopolis.attendance.converters;
+
+import javax.persistence.AttributeConverter;
+import java.sql.Date;
+import java.time.LocalDate;
+
+public class DateConverter implements AttributeConverter<LocalDate, Date> {
+
+    @Override
+    public Date convertToDatabaseColumn(LocalDate attribute) {
+        return attribute != null ? Date.valueOf(attribute) : null;
+    }
+
+    @Override
+    public LocalDate convertToEntityAttribute(Date dbData) {
+        return dbData != null ? dbData.toLocalDate() : null;
+    }
+}
