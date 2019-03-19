ee541439b8cda9dce59e4bbd9794783fa7cee245
a.gromova <a.gromova@gmail.com>
2019-03-19 07:58:17 +0300

    Add frontend

---
+++ frontend/users.js
@@ -0,0 +1,22 @@
+new Vue({
+    el: '.course',
+    data: {
+        options: [
+            { text: 'Introduction to IA'},
+            { text: 'lalal'},
+            { text: 'lolo'}
+        ]
+    }
+})
+
+new Vue({
+    el: '.type',
+    data: {
+        options: [
+            { text: 'Lecture'},
+            { text: 'Tutorial'},
+            { text: 'Lab'}
+        ]
+    }
+})
+
