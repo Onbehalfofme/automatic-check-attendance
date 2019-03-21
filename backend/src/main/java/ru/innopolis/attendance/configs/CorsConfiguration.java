4272bb41ada53695f52e9c53fa731ce07c7f87ee
Maxim Salo <m.salo@innopolis.ru>
2019-03-21 23:57:28 +0300

    Test cors

--- backend/src/main/java/ru/innopolis/attendance/configs/CorsConfiguration.java
+++ backend/src/main/java/ru/innopolis/attendance/configs/CorsConfiguration.java
@@ -1,21 +1,16 @@
 package ru.innopolis.attendance.configs;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfiguration {
+@EnableWebMvc
+public class CorsConfiguration implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
     }
 }
