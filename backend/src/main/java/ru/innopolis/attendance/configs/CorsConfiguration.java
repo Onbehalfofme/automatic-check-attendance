9daf11ee6a535ef3cd999428631aa2f95eee09b4
Maxim Salo <m.salo@innopolis.ru>
2019-03-22 00:22:17 +0300

    Test cors

--- backend/src/main/java/ru/innopolis/attendance/configs/CorsConfiguration.java
+++ backend/src/main/java/ru/innopolis/attendance/configs/CorsConfiguration.java
@@ -1,16 +1,21 @@
 package ru.innopolis.attendance.configs;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.stereotype.Component;
 
-@Configuration
-@EnableWebMvc
-public class CorsConfiguration implements WebMvcConfigurer {
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CorsConfiguration implements Filter {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) res;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
+        chain.doFilter(req, res);
     }
 }
