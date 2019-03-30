1e45c82ea2656bd88975eba8de94abb1d6c6dd8c
Rikitariko <leoprogbars@gmail.com>
2019-03-30 20:14:35 +0300

    Add new component "ToolBar" and ability to create new lecture

--- frontend/src/components/ToolBar.vue
+++ frontend/src/components/ToolBar.vue
@@ -3,21 +3,24 @@
     <div class="list-option">
       <div class="headline">Lecture creation</div>
       <div><h3>Course</h3></div>
-      <select class="course">
-        <option v-for="option in options" v-bind:key="option.value">
-          {{ option.text }}
+      <select class="course" v-model="courseId">
+        <option v-for="option in options" v-bind:value="option.id">
+          {{ option.name }}
         </option>
       </select>
 
       <div><h3>Type</h3></div>
-      <select class="type">
+      <select class="type" v-model="lectureType">
         <option v-for="option in options2" v-bind:key="option.value">
           {{ option.text }}
         </option>
       </select>
 
-      <div><h3>Data</h3></div>
-      <input class="date" type="date" lang="en" name="calendar" />
+      <div><h3>Room</h3></div>
+      <input class="date" type="number" size="3" min="100" max="500" v-model="lectureRoom"/>
+
+      <div><h3>Date</h3></div>
+      <input class="date" type="date" lang="en" name="calendar" v-model="lectureDate"/>
 
       <div><h3>Time of beginning</h3></div>
       <input
@@ -27,19 +30,62 @@
         value="09:00"
         min="00:01"
         max="23:59"
+        v-model="lectureTime"
       />
-      <button>Create</button>
+      <button v-on:click="getUsers(courseId, lectureType, lectureRoom, lectureDate, lectureTime)">Create</button>
     </div>
   </div>
 </template>
 
 <script>
+//import dataService from "../services/dataService.js";
+import axios from "axios";
+import moment from "moment";
+
 export default {
   data() {
     return {
-      options: [{ text: "Introduction to AI" }],
-      options2: [{ text: "Lecture" }, { text: "Tutorial" }, { text: "Lab" }]
+      options: null,
+      options2: [{ text: "Lecture" }, { text: "Tutorial" }, { text: "Lab" }],
+      courseId: null,
+      lectureType: null,
+      lectureDate: new Date(),
+      lectureTime: null,
+      lectureRoom: null
     };
+  },
+
+  created: function() {
+    //dataService.getCourses();
+    //this.options = this.courses;
+    const AXIOS = axios.create({
+      baseURL: "http://134.209.227.130:8080",
+      headers: {
+        Authorization: "JWT " + localStorage.getItem("token"),
+        "Content-Type": "application/json; charset=UTF-8",
+        "Access-Control-Allow-Origin": "*"
+      }
+    });
+
+    AXIOS.get("/courses/enrolled").then(response => {
+      this.options = response.data;
+    });
+  },
+
+  methods: {
+    getUsers: function(courseId, type, room, lectureDate, lectureTime) {
+      const AXIOS = axios.create({
+        baseURL: "http://134.209.227.130:8080",
+        headers: {
+          Authorization: "JWT " + localStorage.getItem("token"),
+          "Content-Type": "application/json; charset=UTF-8",
+          "Access-Control-Allow-Origin": "*"
+        }
+      });
+      let dateTime = moment(lectureDate).format("DD.MM.YYYY") + " " + lectureTime;
+
+      AXIOS.post("/lesson/create", { courseId, dateTime, room, type });
+    }
   }
 };
 </script>
--- frontend/src/services/dataService.js
+++ frontend/src/services/dataService.js
@@ -1,17 +1,27 @@
 import axios from "axios";
-export function getCourses() {
-  const AXIOS = axios.create({
-    baseURL: "http://134.209.227.130:8080",
-    headers: {
-      Authorization: "JWT " + localStorage.getItem("token"),
-      "Content-Type": "application/json; charset=UTF-8",
-      "Access-Control-Allow-Origin": "*"
-    }
-  });
 
-  AXIOS.get("/courses/enrolled").then(response => {
-    concole.log(response.data);
-  });
+export default {
+  data() {
+    return {
+      courses: null
+    };
+  },
 
-  return this.newVar;
-}
+  getCourses() {
+    const AXIOS = axios.create({
+      baseURL: "http://134.209.227.130:8080",
+      headers: {
+        Authorization: "JWT " + localStorage.getItem("token"),
+        "Content-Type": "application/json; charset=UTF-8",
+        "Access-Control-Allow-Origin": "*"
+      }
+    });
+
+    AXIOS.get("/courses/enrolled").then(response => {
+      this.courses = response.data;
+      return response.data;
+    });
+
+    return this.courses;
+  }
+};
