73114c8125cb8d7a6ea3f248265a59a4b557e0c5
Maxim Salo <m.salo@innopolis.ru>
2019-04-02 10:08:26 +0300

    Try to fix date format again

--- backend/src/main/java/ru/innopolis/attendance/DTOs/LogInResponseDTO.java
+++ backend/src/main/java/ru/innopolis/attendance/DTOs/LogInResponseDTO.java
@@ -1,10 +1,10 @@
 package ru.innopolis.attendance.DTOs;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
+@AllArgsConstructor
 public class LogInResponseDTO {
 
     private String token;
