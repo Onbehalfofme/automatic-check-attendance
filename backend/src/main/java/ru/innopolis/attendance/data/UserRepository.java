4dff55dd5bfb9cef86c53da7c8178e720732a109
Maxim Salo <m.salo@innopolis.ru>
2019-04-14 19:39:13 +0300

    Refactor names of models

--- backend/src/main/java/ru/innopolis/attendance/data/UserRepository.java
+++ backend/src/main/java/ru/innopolis/attendance/data/UserRepository.java
@@ -1,9 +1,12 @@
 package ru.innopolis.attendance.data;
 
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.innopolis.attendance.models.UserProfile;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -16,4 +19,6 @@ public interface UserRepository extends JpaRepository<UserProfile, Long> {
     Optional<UserProfile> findByEmail(String email);
 
     boolean existsByEmail(String email);
+
+    List<UserProfile> findAll(Specification<UserProfile> specification, Sort sort);
 }
---
+++ backend/src/main/java/ru/innopolis/attendance/specifications/UserProfileSpecifications.java
@@ -0,0 +1,50 @@
+package ru.innopolis.attendance.specifications;
+
+import org.springframework.data.jpa.domain.Specification;
+import ru.innopolis.attendance.models.Role;
+import ru.innopolis.attendance.models.UserProfile;
+import ru.innopolis.attendance.models.UserProfile_;
+
+import java.time.LocalDate;
+import java.util.Collection;
+
+public class UserProfileSpecifications {
+
+    public static Specification<UserProfile> getUserWithRoles(Collection<Role> roles) {
+        return (Specification<UserProfile>) (root, query, criteriaBuilder) ->
+                roles == null ? null :
+                        criteriaBuilder.isTrue(root.get(UserProfile_.role).in(roles));
+    }
+
+    public static Specification<UserProfile> getUserWithGroupNumbers(Collection<Short> groups) {
+        return (Specification<UserProfile>) (root, query, criteriaBuilder) ->
+                groups == null ? null :
+                        criteriaBuilder.isTrue(root.get(UserProfile_.groupNumber).in(groups));
+    }
+
+    public static Specification<UserProfile> getUserWithBirthdayAfter(LocalDate birthday) {
+        return (Specification<UserProfile>) (root, query, criteriaBuilder) ->
+                birthday == null ? null :
+                        criteriaBuilder.greaterThanOrEqualTo(root.get(UserProfile_.birthday), birthday);
+    }
+
+    public static Specification<UserProfile> getUserWithBirthdayBefore(LocalDate birthday) {
+        return (Specification<UserProfile>) (root, query, criteriaBuilder) ->
+                birthday == null ? null :
+                        criteriaBuilder.lessThanOrEqualTo(root.get(UserProfile_.birthday), birthday);
+    }
+
+    public static Specification<UserProfile> getUserWithFirstName(String firstName) {
+        return (Specification<UserProfile>) (root, query, criteriaBuilder) ->
+                firstName == null ? null :
+                        criteriaBuilder.like(root.get(UserProfile_.name),
+                                "%" + firstName.toLowerCase() + "%");
+    }
+
+    public static Specification<UserProfile> getUserWithLastName(String surname) {
+        return (Specification<UserProfile>) (root, query, criteriaBuilder) ->
+                surname == null ? null :
+                        criteriaBuilder.like(root.get(UserProfile_.surname),
+                                "%" + surname.toLowerCase() + "%");
+    }
+}
