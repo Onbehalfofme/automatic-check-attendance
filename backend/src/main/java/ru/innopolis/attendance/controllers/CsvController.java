4a66357de501b33d82a324df0f6fad9df7f4ad16
Maxim Salo <m.salo@innopolis.ru>
2019-05-13 18:31:19 +0300

    + Add save as CSV

--- backend/src/main/java/ru/innopolis/attendance/controllers/CsvController.java
+++ backend/src/main/java/ru/innopolis/attendance/controllers/CsvController.java
@@ -66,6 +66,7 @@ public class CsvController {
                                 .getAttendance());
                     }
                 }
+                printer.println();
             }
         }
     }
